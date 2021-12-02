23d743f591542d8b62c30da8188afb032e3a8071
merveongun <mongun.dev@gmail.com>
2021-12-02 16:35:16 +0100

    Service registry initial commit

---
+++ service-registry/src/test/java/dev/mongun/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mongun.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
